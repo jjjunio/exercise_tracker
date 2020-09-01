1cc5d41e8f15cce3f47d7c727dd9e41b5a450a8a
jjjunio <jjjunio@gmail.com>
2020-09-01 12:16:42 -0400

    creating Edit-Exercise, Create User and Exercises-list components

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -25,6 +26,10 @@ export default class CreateUser extends Component {
     };
 
     console.log(newUser);
+
+    axios
+      .post("http://localhost:5000/users/add", newUser)
+      .then((res) => console.log(res.data));
 
     this.setState({
       username: "",
